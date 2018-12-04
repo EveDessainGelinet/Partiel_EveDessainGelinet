2f952859e3fa06ddfee28102a0c89e60584c7b21
Eve <eve.dessain-gelinet@etu-webschoolfactory.fr>
2018-12-04 12:00:06 +0100

    config pipeline heroku

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
+	"os"
 	"wsf/devops/handler"
 )
 
 func main() {
 
-	myHandler := handler.NewHandler()
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: myHandler,
+	connection := os.Getenv("DATABASE_URL")
+	fmt.Println(connection)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
-
+	myHandler := handler.NewHandler()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: myHandler,
+		// ReadTimeout:  10 * time.Second,
+		// WriteTimeout: 10 * time.Second,
+	}
+	fmt.Println("Server is running at", port)
 	log.Fatal(server.ListenAndServe())
 }
